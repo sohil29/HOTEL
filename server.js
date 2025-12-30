2225e74161baf6c97a9440f292832137519d4690
sohil29 <sohilprajapat952@gmail.com>
2025-12-31 00:39:33 +0530

    Clean up server.js by removing git commands
    Removed unnecessary git commands and README modifications.

--- server.js
+++ server.js
@@ -8,21 +8,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`SERVER HAS STARTED AT PORT ${PORT}`);
 });
-
-
-
-
-
-echo "# HOTEL" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/sohil29/HOTEL.git
-git push -u origin main
-
-
-
-git remote add origin https://github.com/sohil29/HOTEL.git
-git branch -M main
-git push -u origin main
